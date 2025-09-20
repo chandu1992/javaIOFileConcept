d788974b414179d19aac6a217583c974fbad70e0
chandrashekharbhaupande <chpande55@gmail.com>
2025-09-20 23:16:02 +0530

    file is use for files and folders to create

--- src/main/java/com/jarvis/java_io/fileIOOperation/FileOperations.java
+++ src/main/java/com/jarvis/java_io/fileIOOperation/FileOperations.java
@@ -7,12 +7,22 @@ public class FileOperations {
     public static void main(String[] args) throws IOException {
 
         File f = new File("/Users/chandrashekharbhaupande/Documents/FileIOOperatio/javaiocode.txt");
-        System.out.println(f.exists()); // false
+        System.out.println(f.exists()); // false (just create file object not javaiocode.txt create)
 
-        f.createNewFile();
+        f.createNewFile();  // create javaiocode.txt created
         System.out.println(f.exists()); // true
 
-        f.delete();
+        f.delete();  // delete javaiocode
         System.out.println(f.exists()); // false
+
+
+        File f1 = new File("/Users/chandrashekharbhaupande/Documents/FileIOOperatio/myFolder");
+        System.out.println(f1.exists());  // false
+
+        f1.mkdir();
+        System.out.println(f1.exists());  // true myfolder is create using f1.mkdir
+
+        f1.delete();
+        System.out.println(f1.exists()); // false myFolder is deleted
     }
 }
