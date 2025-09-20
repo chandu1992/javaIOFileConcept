21feaf50f017f5f14c7da4b063cb60ac7786072b
chandrashekharbhaupande <chpande55@gmail.com>
2025-09-20 22:53:37 +0530

    file io operations

---
+++ src/main/java/com/jarvis/java_io/JavaIoApplication.java
@@ -0,0 +1,13 @@
+package com.jarvis.java_io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaIoApplication.class, args);
+	}
+
+}
